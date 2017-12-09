514db67213a7fdec3cddbdb811c99bcee0dd653d
Mitch Shelton <>
2017-12-08 23:01:34 -0700

    Added yesterday data. Also changed joke to include yesterday data.

--- bitbot.py
+++ bitbot.py
@@ -1,5 +1,6 @@
 import urllib, json, praw
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+yesterday_url = "https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday"
 valueOld = 1.000
 value = 1.000
 import praw
@@ -7,17 +8,24 @@ import praw
 reddit = praw.Reddit(client_id='', client_secret='', user_agent='', username='', password='')
 print(reddit.read_only)  # Output: False
 submission = reddit.submission(url='https://www.reddit.com/r/test/comments/7ij0vc/bitbot/')
-response = urllib.urlopen(url)    
+response = urllib.urlopen(url)
+yesterday_response = urllib.urlopen(yesterday_url)
 data = json.loads(response.read())
+yesterday_data = json.loads(yesterday_response.read())
 value = data["bpi"]["USD"]["rate_float"]
-cbv = 100 / value 
+yesterday_value = yesterday_data["bpi"].itervalues().next() #Grab rate from previous day
+cbv = 100 / value
 
 while(True):
     valueOld = value
-    response = urllib.urlopen(url)    
+    response = urllib.urlopen(url)
+    yesterday_response = urllib.urlopen(yesterday_url)
     data = json.loads(response.read())
+    yesterday_data = json.loads(yesterday_response.read())
     value = data["bpi"]["USD"]["rate_float"]
+    yesterday_value = yesterday_data["bpi"].itervalues().next()
+    cbv = 100/value
     if valueOld != value:
-        joke = "A boy asked his bitcoin investing dad for 100. \nDad: $" + "{0:.2f}".format(valueOld * cbv) + "? What do you need $" + "{0:.2f}".format(value * cbv) + " for?\n     (live update to bitcoin value powered by coindesk)"
+        joke = "A boy asked his bitcoin investing dad for $100 worth of bitcoin currency. \nDad: $" + "{0:.2f}".format(valueOld * cbv) + "? What do you need $" + "{0:.2f}".format(value * cbv) + " for? Back in my day $" + "{0:.2f}".format(yesterday_value * cbv) + " could get you " + str(100/yesterday_value) + " of an entire bitcoin.\n     (live update to bitcoin value powered by coindesk)"
         submission.edit(joke)
         print joke
